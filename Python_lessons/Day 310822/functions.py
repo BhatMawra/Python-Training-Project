d5cd0ab4a0e07b847946a70439603f688eebffff
BhatMawra <bhatmawra@gmail.com>
2022-08-31 21:18:23 +0530

    Factorial of a number

---
+++ Python_lessons/Day 310822/functions.py
@@ -0,0 +1,23 @@
+# Program 1 ----> Addition of two numbers
+
+def sum(x,y):
+
+    z=x+y
+    return z
+
+result=sum(10,20) 
+print(result)  
+
+
+#Program 2 ---->Factorial of a Number
+import math
+
+def factorial(n):
+    return(math.factorial(n))
+
+n=int(input("Enter the Number: "))   
+print("Factorial of", n, "is",factorial(n)) 
+
+
+ 
+
