a94cacb536505baef5320fdc9098eb4a9ebc30ce
BhatMawra <bhatmawra@gmail.com>
2022-10-05 19:33:53 +0530

    New topics

---
+++ Python_lessons/Day 031022/scraper.py
@@ -0,0 +1,11 @@
+# import requests
+# from bs4 import BeautifulSoup
+
+# req = requests.get("https://pypi.org/project/beautifulsoup4/")
+
+# # print(req.content)
+
+# soup = BeautifulSoup(req.content,"html.parser")
+# print(soup.title.get_text())
+
+
--- Python_lessons/Day 200922/main.py
+++ Python_lessons/Day 200922/main.py
@@ -1,22 +1,27 @@
-
-f = open("E:\Python-Training-Project\Python_lessons\Day 200922\exercise.txt","r")
-
-
-
 class Employee:
-  no_of_leaves = 10
+    no_of_leaves = 10
 
-
-
-def __init__(self,name,age):
+    def __init__(self,name,age):
         self.name = name
         self.age = age
-with open("E:\Python-Training-Project\Python_lessons\Day 200922\exercise.txt") as f:
-         
-        def printdetails(self):
-          print("Employee ",self.name,"has ", self.no_of_leaves," leaves his age is ",self.age)
+    def printdetails(self):
+                print("Employee ",self.name,"has ", self.no_of_leaves," leaves his age is ",self.age)
+    # emp1=Employee(readlines())
+    
+file = open('exercise.txt', "r")
+content = file.readlines()
 
-# emp1=Employee(readlines())
-# emp1.printdetails()
+employees = []
+i =0
+emp = [0,0,0]
+for line in content:
+    name, age = line.strip().split(',')
 
+    # employees.append({'name': name, 'age': int(age)})
+    emp[i] = Employee(name,age)
+    emp[i].printdetails()
+    i+=1
 
+# print(employees)
+
+file.close()
